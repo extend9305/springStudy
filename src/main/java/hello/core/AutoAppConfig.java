589369dd63c8acde88d4a92db846477e5e5c209f
extend9305 <extend9305@naver.com>
2022-02-17 22:44:09 +0900

    2022-02-17 study

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // filter를하는데 AppConfig 뺴려고 적음
+        //basePackages = "hello.core.member", //--> 지정한 패키지 뒤짐
+        //--> 아무것도 지정하지 않으면 componentScan 붙은 이파일의 패키지(hello.core)에 있는걸 뒤짐짐        // filter를하는데 AppConfig 뺴려고 적음
+        //--> 권장은 config파일을 프로젝트 최상단 패키지경로에 넣고 사용.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+
 )
 public class AutoAppConfig {
 
