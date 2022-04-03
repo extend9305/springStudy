6f8c0fe00b8566a9cbac332ee86cdf2df3c77322
extend9305 <extend9305@naver.com>
2022-04-03 13:24:53 +0900

    2022-04-03 study-2

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,10 +1,8 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
 // 잘사용 X
-public class NetworkClient implements InitializingBean , DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -37,14 +35,12 @@ public class NetworkClient implements InitializingBean , DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override   //생성자생성후
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override //빈 해제 직후
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         disconnect();
     }
 }
